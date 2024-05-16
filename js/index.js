744f2083f509039cb32a80ba8a2112701f7dbbca
shuhratormonaliyev <shuratormonaliyev7@gmail.com>
2024-05-16 23:07:22 +0500

    script

---
+++ js/index.js
@@ -0,0 +1,140 @@
+// // 1-misol
+
+// let l = +prompt('Uzunlik L santimetrda berilgan. Undagi toliq metrlar sonini aniqlovchi programma tuzilsin. (1m-100cm)');
+
+// let a = Math.floor(l/100);
+
+// console.log(a);
+
+// 2-misol
+
+// let t = +prompt( 'Ogirlik M kilogramda berilgan. Undagi toliq tonnalar sonini aniqlovchi programma tuzilsin. (1t=1000kg)');
+
+// let m = Math.floor(t/1000);
+
+// console.log(m);
+// 
+// // 3-miosl 
+
+// let n = +prompt('Faylning hajmi baytlarda berilgan. Bolib butunni olish operatsiyasidan foydalanib fayl hajmining toliq kilobaytlarda ifodalovchi programma tuzilsin. (1Kb=1024 bayt)');
+
+// let kb = Math.floor(n^10);
+
+// console.log(kb);
+
+// // 4-misol
+
+// let enter = prompt('A va B (A > B) musbat sonlari berilgan. A kesmada, B kesmani necha marta joylashtirish mumkinligini aniqlovchi programma tuzilsin.'); 
+// let A = +prompt('A ni kiriting');
+// let B = +prompt('B ni kiriting');
+
+// let RESULT = Math.floor('A/B');
+
+// console.log(RESULT);
+
+// // 5-misol
+// let enter = prompt ('A va B (A > B) musbat sonlar berilgan A kesmada B kesmani necha marta joylashtirish mumkin A kesmada B kesmaning joylashmagan qismini aniqlovchi programma tuzilsin');
+// let A = +prompt('A ni kiriting');
+// let B = +prompt('B ni kiriting');
+// let RESULT = Math.floor('A-B');
+// console.log('RESULT');
+// 6-misol
+// let a = +prompt ('Ikki xonali son berilgan Oldin uning onliklar xonasidagi raqamni song birlar xonasidagi raqamni chiqaruvchi programma tuzilsin');
+// let onliklar = Math.floor(a/10);
+
+// let birliklar = Math.floor(a%10);
+
+// console.log(onliklar);
+
+// console.log(birliklar);
+
+// 7-misol
+// 
+
+// 8-misol
+
+// let a = +prompt ('Ikki xonali son berilgan Uning raqamlari ornini almashtirishdan hosil bolgan sonni aniqlovchi programma tuzilsin');
+
+// let n = Math.floor(a%10);
+
+// let m = Math.floor(a/10);
+
+// let l = Math.floor(n*10+m);
+
+// console.log (l);
+
+// 9-misol
+// let y = +prompt ('Uch xonali son berilgan Uning yuzlar xonasidagi raqamini aniqlovchi programma tuzilsin');
+// let result = Math.floor((y/100)%10);
+// console.log(result);
+
+// 10-misol 
+// let c = +prompt ('Uch xonali son berilgan Oldin uni birliklar xonasidagi raqamni song onliklar xonasidagi raqamni chiqaruvchi programma tuzilsin');
+ 
+// let a = Math.floor(c%10);
+
+// let b = Math.floor((c/10)%10);
+
+// console.log(a);
+// console.log(b);
+
+
+// 11-misol
+
+// let p = prompt('Uch xonali son berilgan Uning raqamlar yigindisini aniqlovchi programma tuzilsin');
+
+// let a = +prompt ('birinchi sonni kiriting');
+// let b = +prompt ('ikkinchi sonni kiriting');
+// let c = +prompt ('uchinchi sonni kiriting');
+
+// let y = Math.floor((a+b+c)/3);
+
+// console.log(y);
+
+// 12-misol 
+
+// let k = prompt('Uch xonali son berilgan Uning raqamlarini teskari tartibda yozishdan hosil bolgan sonni aniqlovchi program tuzilsin');
+
+// let a = +prompt ('birinchi sonni kiriting');
+// let b = +prompt ('ikkinchi sonni kiriting');
+// let c = +prompt ('uchinchi sonni kiriting');
+
+// let A = Math.floor(a%10);
+// let B = Math.floor((b/10)%10);
+// let C = Math.floor(c/100);
+// let Y = Math.floor(c*100+b*10+a);
+// console.log(Y);
+
+// 13-misol 
+
+// let k = prompt('Uch xonali son berilgan. Uning chapdan birinchi raqamini ochirib ong tarafiga yozishdan hosil bolgan sonni aniqlovchi programma tuzilsin')
+
+// let a = +prompt ('birinchi sonni kiriting');
+// let b = +prompt ('ikkinchi sonni kiriting');
+// let c = +prompt ('uchinchi sonni kiriting');
+
+// let A = Math.floor(a%10);
+// let B = Math.floor((b/10)%10);
+// let C = Math.floor(c/100);
+// let Y = Math.floor(b*100+a*10+c);
+// console.log(Y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
